0b6180b65f1e3a0f620e229a8e93c053c3dc9277
Niklas Tangen <niklas.tangen@hs-osnabrueck.de>
2022-05-16 13:09:47 +0200

    GUI Anamnesebogen (16.05. 13:00)

--- BeFit/Erstgespräch.xaml.cs
+++ BeFit/Erstgespräch.xaml.cs
@@ -23,5 +23,10 @@ namespace BeFit
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
