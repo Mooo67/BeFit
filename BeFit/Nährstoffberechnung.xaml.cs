c06d166f4836ce32fb6280f2ce01f2e21b7507b8
Jan Lorenz <77906722+lorenzjan@users.noreply.github.com>
2022-05-16 17:25:48 +0200

    Add files via upload

---
+++ BeFit/Nährstoffberechnung.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BeFit
+{
+    /// <summary>
+    /// Interaktionslogik für Nährstoffberechnung.xaml
+    /// </summary>
+    public partial class Nährstoffberechnung : Window
+    {
+        public Nährstoffberechnung()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Herzlichen Glückwunsch, du bist Fett";
+            MessageBox.Show(message);
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BeFit/Window1.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BeFit
+{
+    /// <summary>
+    /// Interaktionslogik für Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            var myForm = new BeFit.Kunden();
+            myForm.Show();
+        }
+
+        public void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            var myForm = new BeFit.Trainingsplan();
+            myForm.Show();
+        }
+
+        public void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            var myForm = new BeFit.Nährstoffberechnung();
+            myForm.Show();
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            var myForm = new BeFit.BMI_Rechner();
+            myForm.Show();
+        }
+
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            var myForm = new BeFit.Erstgespräch();
+            myForm.Show();
+        }
+
+        private void Button_Click7(object sender, RoutedEventArgs e)
+        {
+        //Support
+        }
+
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+            //Logout
+            this.Visibility = Visibility.Hidden;
+            string message = "Ausgeloggt";
+            MessageBox.Show(message);
+            this.Close();
+        }
+    }
+}
